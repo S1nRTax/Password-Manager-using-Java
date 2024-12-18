ddd56ca361631de9dd03ad55af0eade1bbbfee6d
S1nRTax <badreddine.asaad4@gmail.com>
2024-12-18 22:49:47 +0100

    Implemented a simple GUI

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,5 +1,24 @@
 package gui;
 
-public class GUI {
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 
+
+
+public class GUI extends JFrame{
+
+        // constructor:
+        public GUI(){
+            this.setTitle("Password Manager.");
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.setResizable(false);
+            this.setSize(500, 500);
+            this.setVisible(true);
+
+            ImageIcon image = new ImageIcon("../passwordManager.png"); // create an Image icon
+            this.setIconImage(image.getImage()); //set it to the frame
+
+            this.getContentPane().setBackground(Color.DARK_GRAY);// set the background color to dark gray.
+        }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,17 @@
 package main;
 
+
+import db.DatabaseManager;
+import gui.GUI;
+
 public class Main {
 
+    public static void main(String[] args) {
+        // initialize db.
+        DatabaseManager.initializeDB();
+
+        // start the GUI.
+        GUI frame = new GUI();
+    }
+    
 }
